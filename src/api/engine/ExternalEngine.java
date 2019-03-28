ac76b50aa34e0bd3c669db698d4757780f493f23
Anna Darwish <ard51@duke.edu>
2019-03-28 17:45:54 -0400

    Deleted visual state as we decided a different form of communication between back end and front end

--- src/api/engine/ExternalEngine.java
+++ src/api/engine/ExternalEngine.java
@@ -7,6 +7,6 @@ public interface ExternalEngine {
      * Updates all GameObjects that were involved in some event that requires values to change. This method will be called
      * as a "step" function would
      */
-    VisualState updateState(Collection<Integer> myInputKeys);
+    void updateState(Collection<Integer> myInputKeys);
 
 }
--- src/api/engine/VisualState.java
+++
@@ -1,7 +0,0 @@
-package api.engine;
-
-import java.util.Collection;
-
-public abstract class VisualState {
-    abstract Collection<GameObject> getGameObjects();
-}
