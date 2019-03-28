100d8be4068dc44c65576bbb1d5ff078c2dce470
Anna Darwish <ard51@duke.edu>
2019-03-28 17:52:09 -0400

    changed return types of data api for loading in games

--- src/api/data/ExternalData.java
+++ src/api/data/ExternalData.java
@@ -6,7 +6,7 @@ public interface ExternalData {
      * necessarily need access to all of the game's information so there will be a more encapsulated version that simply
      * stores information about top plays of the game, a display image, and the name of the game
      */
-    void loadGameDisplay(String gameName);
+    Object loadGameDisplay(String gameName);
     /*
      * From the game center, a particular game's display may be edited to the user's preference. For example, a game's
      * display image may change from some default background to a likely scene in that particular game. This method will
@@ -20,14 +20,14 @@ public interface ExternalData {
      * files will be separated into ones that the runner needs access to (music, background image, etc.)  and particular
      * state and behavior information that the engine will need access to in order to evaluate logic.
      */
-    void loadGame(String gameName);
+    Object loadGame(String gameName);
     /*
      * This method will continue a game from its most recent state (i.e. if the user had gotten to a saved checkpoint, exited
      * the game, played another one, and then wished to return to the first game, they may do so by invoking the following
      * method. It may call the same methods as loadGame for the most part and simply include one or two more xml files
      * to specify where the game last left off
      */
-    void continueGame(String gameName);
+    Object continueGame(String gameName);
     /*
      * This method will save the current state of the game (if the user has reached a checkpoint, it will store the current
      * score, time, hero lives, hero position, etc. so that the user may continue wherever they left off if they choose to
