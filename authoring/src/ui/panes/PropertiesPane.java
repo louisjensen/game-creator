8687ab2fa4a5517e344cc52a4e1e4e79212ef3db
Harry Ross <hgr8@duke.edu>
2019-04-01 19:29:39 -0400

    More detail into properties

--- authoring/src/ui/panes/PropertiesPane.java
+++ authoring/src/ui/panes/PropertiesPane.java
@@ -1,31 +1,63 @@
 package ui.panes;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 
-import java.util.List;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
 
 class PropertiesPane extends TitledPane {
 
-    private static final String PROP_TYPE_EXT = " Properties";
+    private String myObjectLabel; //Or use hte GameObject itself depending on the backend
 
-    PropertiesPane(String type, List<String> list) {
-        GridPane optionsPane = createGridPane(list);
+    private static final String PROP_TYPE_EXT = " Properties";
+    private static final String PROP_TYPE_FILE = "object_properties_list";
+
+    PropertiesPane(String type, String typeName) {
+        GridPane optionsPane = createPropertiesGrid(typeName);
         this.setText(type + PROP_TYPE_EXT);
         this.setContent(optionsPane);
     }
 
-    private GridPane createGridPane(List<String> options) {
+    private GridPane createPropertiesGrid(String typeName) {
         GridPane gridlist = new GridPane();
-        for (int i = 0; i < options.size(); i++) {
-            HBox newBox = new HBox();
 
-            newBox.getChildren().add(new Text(options.get(i)));
-            gridlist.add(newBox, 0, i);
+        ResourceBundle bundle = ResourceBundle.getBundle(PROP_TYPE_FILE);
+        Enumeration propNames = bundle.getKeys();
+
+        while (propNames.hasMoreElements()) {
+            String name = (String)propNames.nextElement();
+            String value = bundle.getString(name);
+            gridlist.add(createProperty(name, value), 0, gridlist.getRowCount());
         }
+
+        //get text fields from resource bundle, populate current values from GameObject
         return gridlist;
+    }
+
+    private VBox createProperty(String name, String value) {
+        VBox newProp = new VBox();
+
+        Label propName = new Label(name);
+        propName.getStyleClass().add("prop-label");
+
+        //parse value for colon
+        //create new XXXXX, populate with value rom GameObject
+        //set up bind
+
+        newProp.getChildren().addAll(propName, populateOptions(propName.getText()));
+
+        return newProp;
+    }
+
+    private HBox populateOptions(String label) {
+        HBox propOptions = new HBox();
+        propOptions.getChildren().add(new TextField());
+
+        return propOptions;
     }
 }
