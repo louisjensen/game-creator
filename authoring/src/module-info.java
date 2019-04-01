bf5372177f4f208003530bf335eca4f5acf64deb
Harry Ross <hgr8@duke.edu>
2019-04-01 16:52:51 -0400

    Added some initial pane classes and properties files, began work on PropertiesPane

--- authoring/src/module-info.java
+++ authoring/src/module-info.java
@@ -1,6 +1,11 @@
 module authoring {
     requires runner;
     requires data;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
 
     exports authoring.external;
+    exports ui.panes;
 }
--- authoring/src/ui/panes/PropertiesPane.java
+++ authoring/src/ui/panes/PropertiesPane.java
@@ -1,4 +1,31 @@
 package ui.panes;
 
-public class PropertiesPane {
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+import java.util.List;
+
+class PropertiesPane extends TitledPane {
+
+    private static final String PROP_TYPE_EXT = " Properties";
+
+    PropertiesPane(String type, List<String> list) {
+        GridPane optionsPane = createGridPane(list);
+        this.setText(type + PROP_TYPE_EXT);
+        this.setContent(optionsPane);
+    }
+
+    private GridPane createGridPane(List<String> options) {
+        GridPane gridlist = new GridPane();
+        for (int i = 0; i < options.size(); i++) {
+            HBox newBox = new HBox();
+
+            newBox.getChildren().add(new Text(options.get(i)));
+            gridlist.add(newBox, 0, i);
+        }
+        return gridlist;
+    }
 }
