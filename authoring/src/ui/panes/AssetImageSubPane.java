3d71601414ef6e58e96c0f552fd37c54f8e1a1fc
Carrie Hunner <clh87@duke.edu>
2019-04-02 21:32:27 -0400

    #53 formatted subpanes to be more uniform

--- authoring/src/ui/panes/AssetImageSubPane.java
+++ authoring/src/ui/panes/AssetImageSubPane.java
@@ -1,17 +1,25 @@
 package ui.panes;
 
-import javafx.scene.control.TitledPane;
+import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class AssetImageSubPane extends TitledPane {
-    private static final int IMAGE_SIZE = 20;
+public class AssetImageSubPane extends VBox {
+    private static final int IMAGE_SIZE = 50;
+    private static final int PANE_SIZE = IMAGE_SIZE + 10;
 
     public AssetImageSubPane(String title, ImageView image){
-        this.setText(title);
-        this.setContent(image);
+        this.setMaxSize(PANE_SIZE, PANE_SIZE);
+        this.setPrefSize(PANE_SIZE, PANE_SIZE);
         image.setFitWidth(IMAGE_SIZE);
         image.setFitHeight(IMAGE_SIZE);
-        this.setCollapsible(false);
+
+
+        Text text = new Text(title);
+        this.setAlignment(Pos.CENTER);
+        this.getChildren().add(image);
+        this.getChildren().add(text);
     }
 
 }
