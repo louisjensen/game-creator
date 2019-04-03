8083b6d6ab4aa0bee61476acc8f4edcbab73831e
Carrie Hunner <clh87@duke.edu>
2019-04-02 20:48:04 -0400

    #53 added protection for if a file doesnt have an extension or it is not jpg or png

--- authoring/src/ui/manager/AssetManager.java
+++ authoring/src/ui/manager/AssetManager.java
@@ -5,6 +5,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import ui.TestEntity;
 import ui.panes.AssetImageSubPane;
@@ -12,15 +13,28 @@ import ui.panes.AssetImageSubPane;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
 
 public class AssetManager extends Stage {
 
     private TestEntity myEntity;
+    private ResourceBundle myResources;
+    private static final String BUNDLE_NAME = "AssetManager";
+    private Set<String> myImageExtensions;
+    private String EXTENSION_RESOURCE_KEY = "AcceptableImageExtensions";
 
     public AssetManager(TestEntity entity) {
         myEntity = entity;
-        BorderPane bp = new BorderPane();
-        Scene scene = new Scene(bp);
+        myResources = ResourceBundle.getBundle(BUNDLE_NAME);
+        myImageExtensions = new HashSet<>();
+        fillImageExtensionSet();
+
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
         this.setScene(scene);
         this.setResizable(false);
         HBox hbox = new HBox();
@@ -28,18 +42,36 @@ public class AssetManager extends Stage {
         File assetFolder = new File("authoring/Assets/Images");
         System.out.println("Made it past filepath");
         for(File temp : assetFolder.listFiles()){
-            System.out.println(temp.getPath());
-            ImageView imageView = new ImageView();
-            Image image = null;
+            System.out.println(temp.getName());
             try {
-                image = new Image( new FileInputStream(temp.getPath()));
-            } catch (FileNotFoundException e) {
-                //TODO DEAL WITH THIS
+                String extension = temp.getName().split("\\.")[1];
+                if(myImageExtensions.contains(extension)){
+                    ImageView imageView = new ImageView();
+                    Image image = null;
+                    try {
+                        image = new Image( new FileInputStream(temp.getPath()));
+                    } catch (FileNotFoundException e) {
+                        //TODO DEAL WITH THIS
+                    }
+                    imageView.setImage(image);
+                    AssetImageSubPane subPane = new AssetImageSubPane(temp.getName().split("\\.")[0], imageView);
+                    hbox.getChildren().add(subPane);
+                }
             }
-            imageView.setImage(image);
-            AssetImageSubPane subPane = new AssetImageSubPane(temp.getName(), imageView);
-            hbox.getChildren().add(subPane);
+            catch (IndexOutOfBoundsException e){
+                //this occurs when there is no file extension
+                //this file should be skipped over and nothing should happen
+                //this catch should be empty
+            }
+
+
         }
-        bp.setCenter(hbox);
+        vbox.getChildren().add(hbox);
+    }
+
+    private void fillImageExtensionSet(){
+        for(String s: myResources.getString(EXTENSION_RESOURCE_KEY).split(",")){
+            myImageExtensions.add(s);
+        }
     }
 }
