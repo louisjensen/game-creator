b79947cc7fdfe96ef902d2ace87c9e1b185f5b6e
Louis Jensen <lbj7@duke.edu>
2019-04-04 12:42:23 -0400

    initial window created for runner #47

--- runner/src/runner/internal/RunnerTester.java
+++ runner/src/runner/internal/RunnerTester.java
@@ -1,8 +1,16 @@
 package runner.internal;
 
-public class RunnerTester {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import runner.external.GameRunner;
 
-    public static void main(String[] args){
+public class RunnerTester extends Application {
+
+    @Override
+    public void start (Stage primaryStage) throws Exception{
         System.out.println("testing");
+        GameRunner runner = new GameRunner(null, 200, 200, primaryStage);
+        runner.display();
     }
+
 }
