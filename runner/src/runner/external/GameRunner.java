d6210b45645226333f1d0c032ff69a129396e08d
Louis Jensen <lbj7@duke.edu>
2019-04-04 12:19:09 -0400

    simple consturctor for runner #64

--- runner/src/runner/external/GameRunner.java
+++ runner/src/runner/external/GameRunner.java
@@ -1,8 +1,21 @@
 package runner.external;
 
+import engine.external.Component;
+
+import java.util.List;
+
 public class GameRunner {
     /**
      * This will be the primary class that creates a new game engine
      * and displays sprites on a stage
      */
+    private List<Component> myComponents;
+    private int mySceneWidth;
+    private int mySceneHeight;
+
+    public GameRunner(List<Component> components, int width, int height){
+        myComponents = components;
+        mySceneWidth = width;
+        mySceneHeight = height;
+    }
 }
