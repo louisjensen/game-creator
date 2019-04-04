c02cd535d32dc72702c5a03f711824e669d328ff
Lucas Liu <ltl9@duke.edu>
2019-04-04 15:40:25 -0400

    Event and EventHandlerSystem skeleton

--- engine/src/engine/internal/systems/EventHandlerSystem.java
+++ engine/src/engine/internal/systems/EventHandlerSystem.java
@@ -1,16 +1,27 @@
 package engine.internal.systems;
 
+import engine.external.Event;
 import engine.external.component.Component;
 
 import java.util.Collection;
+import java.util.List;
 
+
+/**
+ * Checks all Events every cycle
+ */
 public class EventHandlerSystem extends System {
+    List<Event> events;
+
+    //TODO: feed Constructor a list of Events, not Components
     public EventHandlerSystem(Collection<Class<? extends Component>> requiredComponents) {
         super(requiredComponents);
     }
 
     @Override
     protected void run() {
-        //TODO: implement this
+        for (Event e : events) {
+            e.execute();
+        }
     }
 }
