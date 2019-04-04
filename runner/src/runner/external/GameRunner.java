1d99dbc7e40bfeec560fa53cd8662ab163da1259
Louis Jensen <lbj7@duke.edu>
2019-04-04 12:14:10 -0400

    Created primary runner class #47

---
+++ runner/src/runner/external/GameRunner.java
@@ -0,0 +1,8 @@
+package runner.external;
+
+public class GameRunner {
+    /**
+     * This will be the primary class that creates a new game engine
+     * and displays sprites on a stage
+     */
+}
