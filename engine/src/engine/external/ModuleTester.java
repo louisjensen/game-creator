ae1f1bc4ae1ae3a2c630c32453fa4dd59fdf690e
Louis Jensen <lbj7@duke.edu>
2019-04-04 12:22:21 -0400

    Removing unecessary testing files

--- engine/src/engine/external/ModuleTester.java
+++ engine/src/engine/external/ModuleTester.java
@@ -3,8 +3,6 @@ package engine.external;
 public class ModuleTester {
     public static void main(String[] args){
         Entity myEntity = new Entity();
-        myEntity.addComponent(new NameComponent<>("Doovall"));
-        myEntity.addComponent(new XPositionComponent<>(10.0));
 
     }
 }
