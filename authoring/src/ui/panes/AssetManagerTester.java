5107e62da0b66373740088520cdb95700055e595
Carrie Hunner <clh87@duke.edu>
2019-04-03 20:09:07 -0400

    #53 created a tester for the AuthoringManager - this can eventually be removed

---
+++ authoring/src/ui/panes/AssetManagerTester.java
@@ -0,0 +1,23 @@
+package ui.panes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ui.ErrorBox;
+import ui.TestEntity;
+import ui.UIException;
+import ui.manager.AssetManager;
+
+public class AssetManagerTester extends Application {
+
+    @Override
+    public void start(Stage testStage) {
+        AssetManager temp = new AssetManager(new TestEntity());
+        String selected = temp.showAndReturn("flappy_bird.png");
+        System.out.println("Selected Image: " + selected);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
