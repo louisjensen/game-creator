c8521c196da56bbe9c3a853f322574f64485ef5c
Louis Jensen <lbj7@duke.edu>
2019-04-05 12:42:09 -0400

    added very simple engine for testing sprite movement #46

---
+++ runner/src/runner/internal/TestEngine.java
@@ -0,0 +1,32 @@
+package runner.internal;
+
+import engine.external.Entity;
+import engine.external.PositionComponent;
+import javafx.geometry.Point3D;
+
+import java.util.List;
+
+public class TestEngine {
+    private List<Entity> myEntities;
+
+    public TestEngine(List<Entity> entities){
+        myEntities = entities;
+    }
+
+    public void update(){
+        moveStuffRight();
+    }
+
+    private void moveStuffRight(){
+        for(Entity entity : myEntities){
+            PositionComponent positionComponent = (PositionComponent) entity.getComponent(PositionComponent.class);
+            Point3D position = (Point3D) positionComponent.getValue();
+            double x = position.getX() + 5;
+            double y = position.getY();// + velocityComponent.getValue().getY();
+            double z = position.getZ();
+
+            positionComponent.setValue(new Point3D(x, y, z));
+        }
+    }
+}
+
