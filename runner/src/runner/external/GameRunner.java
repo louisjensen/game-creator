d8a9b3945ee0d826b6716010493ec69e06854bb3
Louis Jensen <lbj7@duke.edu>
2019-04-05 11:40:31 -0400

    Displaying entity objects based on their position component #64

--- runner/src/runner/external/GameRunner.java
+++ runner/src/runner/external/GameRunner.java
@@ -1,12 +1,16 @@
 package runner.external;
 
-import engine.external.Component;
+import engine.external.Entity;
+import engine.external.PositionComponent;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GameRunner {
@@ -14,36 +18,46 @@ public class GameRunner {
      * This will be the primary class that creates a new game engine
      * and displays sprites on a stage
      */
-    private List<Component> myComponents;
+    private List<Entity> myEntities;
     private int mySceneWidth;
     private int mySceneHeight;
     private Stage myStage;
     private Group myGroup;
     private Scene myScene;
+    private Canvas myCanvas;
+    private GraphicsContext myGraphicsContext;
 
-    public GameRunner(List<Component> components, int width, int height, Stage stage){
-       // myComponents = components;
+    public GameRunner(List<Entity> entities, int width, int height, Stage stage){
+        myEntities = entities;
         mySceneWidth = width;
         mySceneHeight = height;
         myStage = stage;
         myGroup = new Group();
-        myScene = new Scene(myGroup, mySceneWidth, mySceneHeight, Color.GREEN);
+        myScene = new Scene(myGroup);
+        myCanvas = new Canvas(mySceneWidth, mySceneHeight);
+        myGroup.getChildren().add(myCanvas);
+        myGraphicsContext = myCanvas.getGraphicsContext2D();
+        myScene.setFill(Color.BEIGE);
+        showEntities(myGraphicsContext);
         myStage.setScene(myScene);
     }
+
+
 
     public void display() {
         myStage.show();
     }
-}
 
-//    public GUIDisplay(Stage stage){
-//        myLanguage = DEFAULT_LANGUAGE;
-//        myStage = stage;
-//        myListOfCommands = new ArrayList<>();
-//        dataTracker = new GUIdata();
-//        myRoot = createGridPane();
-//        myDelegator = new Delegator(myStackedCanvasPane, myTabExplorer, myPaletteTabExplorer, myToolbar);
-//        myRoot.setGridLinesVisible(false);
-//        myScene = new Scene(myRoot, SCENE_WIDTH, SCENE_HEIGHT, Color.LIGHTGRAY);
-//        myStage.setScene(myScene);
-//    }
+    private void showEntities(GraphicsContext graphics){
+        for(Entity entity : myEntities){
+            PositionComponent positionComponent = (PositionComponent) entity.getComponent(PositionComponent.class);
+
+            System.out.println(positionComponent);
+            Point3D position = (Point3D) positionComponent.getValue();
+            Double xPosition = position.getX();
+            Double yPosition = position.getY();
+
+            graphics.drawImage(new Image("basketball.png", 50, 50, true, true), xPosition, yPosition);
+        }
+    }
+}
