95860e56714275e83c797f66fb0a0a1138477f22
Louis Jensen <lbj7@duke.edu>
2019-04-05 11:38:57 -0400

    Modifying component to match updated version #47

--- engine/src/engine/external/Entity.java
+++ engine/src/engine/external/Entity.java
@@ -1,10 +1,13 @@
 package engine.external;
 
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Entity {
-    private Map<Class<?>, Component<?>> myComponents;
+    private Map<Class<? extends Component>, Component<?>> myComponents;
 
     public Entity() {
         myComponents = new HashMap<>();
@@ -17,5 +20,18 @@ public class Entity {
     public void removeComponent(Component<?> component){
         myComponents.remove(component.getClass());
     }
+
+    public boolean hasComponents(Collection<Class<? extends Component>> components) {
+        return myComponents.keySet().containsAll(components);
+    }
+
+    public boolean hasComponents(Class<? extends Component> component) {
+        return hasComponents(Arrays.asList(component));
+    }
+
+    public Component<?> getComponent(Class<? extends Component> clazz) {
+        return myComponents.get(clazz);
+    }
+
 
 }
