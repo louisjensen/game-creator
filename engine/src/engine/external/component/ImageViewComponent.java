a321992d2b604eee886c64d83befd47c9cd85551
Hsingchih Tang <ht114@duke.edu>
2019-04-06 16:22:22 -0400

    Implemented ImageViewSystem to initialize ImageViews for Entities #74

--- engine/src/engine/external/component/ImageViewComponent.java
+++ engine/src/engine/external/component/ImageViewComponent.java
@@ -1,4 +1,15 @@
 package engine.external.component;
 
-public class ImageViewComponent {
+import javafx.scene.image.ImageView;
+
+/**
+ * For storing the visualization for Entities
+ * Not serializable; needs to be created in Engine
+ */
+public class ImageViewComponent extends Component<ImageView>{
+
+    public ImageViewComponent(ImageView imageView){
+        super(imageView);
+    }
+
 }
