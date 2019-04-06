b77bd3bb7873ecdaf7a93cfced2553d44b4ccca8
Megan Phibbons <mep76@duke.edu>
2019-04-05 23:43:38 -0400

    add in the ability to dynamically add multiple cards based on a number of cards. To do: put in content for cards and make them read from a file. Progress on #51

--- center/src/center/external/CenterMain.java
+++ center/src/center/external/CenterMain.java
@@ -9,16 +9,13 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import ui_components.games.GameCard;
+import ui_components.games.GameList;
 import ui_components.header.HeaderBar;
 
 public class CenterMain extends Application {
     private static final double STAGE_WIDTH = 1250;
     private static final double STAGE_HEIGHT = 750;
     private static final Color BACKGROUND_COLOR = Color.rgb(46, 43, 51);
-
-    private static final double CARD_H_OFFSET = 50;
-    private static final double CARD_V_OFFSET = 50;
-    private static final int NUM_CARDS_DISPLAYED = 3;
 
     public static void main(String[] args){
         launch(args);
@@ -32,14 +29,8 @@ public class CenterMain extends Application {
         root.setStyle("-fx-background-color: transparent;");
         layout.setStyle("-fx-background-color: transparent;");
         root.setTop(layout);
-        FlowPane gameList = new FlowPane();
-        GameCard card = new GameCard();
-        GameCard card2 = new GameCard();
-        gameList.setVgap(CARD_V_OFFSET);
-        gameList.setHgap(CARD_H_OFFSET);
-        gameList.setPrefWrapLength((GameCard.DISPLAY_WIDTH + CARD_H_OFFSET) * NUM_CARDS_DISPLAYED);
-        gameList.getChildren().addAll(card.getDisplay(), card2.getDisplay());
-        root.setCenter(gameList);
+        GameList gameList = new GameList(5);
+        root.setCenter(gameList.getDisplay());
         Scene myScene = new Scene(root, STAGE_WIDTH, STAGE_HEIGHT, BACKGROUND_COLOR);
         myStage.setScene(myScene);
         myStage.show();
--- center/src/ui_components/games/GameList.java
+++ center/src/ui_components/games/GameList.java
@@ -1,4 +1,35 @@
 package ui_components.games;
 
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+
 public class GameList {
+    private Pane myDisplay;
+    private ArrayList<String> gameNames = new ArrayList<String>(); // todo: use this to populate cards instead of a number of games
+    private static final double CARD_H_OFFSET = 50;
+    private static final double CARD_V_OFFSET = 50;
+    private static final int NUM_CARDS_DISPLAYED = 3;
+
+    public GameList(int numGames) {
+        initializeDisplay(numGames);
+    }
+
+    public Pane getDisplay() {
+        return myDisplay;
+    }
+
+    private void initializeDisplay(int numGames) {
+        FlowPane gameList = new FlowPane();
+        gameList.setVgap(CARD_V_OFFSET);
+        gameList.setHgap(CARD_H_OFFSET);
+        gameList.setPrefWrapLength((GameCard.DISPLAY_WIDTH + CARD_H_OFFSET) * NUM_CARDS_DISPLAYED);
+        for(int i = 0; i < numGames; i++) {
+            GameCard c = new GameCard();
+            gameList.getChildren().add(c.getDisplay());
+        }
+        myDisplay = gameList;
+    }
+
 }
