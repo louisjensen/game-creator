75e75c1662483199391ba44e138caf7f6f7db926
Carrie Hunner <clh87@duke.edu>
2019-04-06 11:04:46 -0400

    #69 #65 adjusted EntityMenu to be more versatile and updated DefaultTypesPane to accomodate those changes

--- authoring/src/ui/panes/DefaultTypesPane.java
+++ authoring/src/ui/panes/DefaultTypesPane.java
@@ -1,29 +1,32 @@
 package ui.panes;
 
-import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class NewEntityTypePane extends VBox{
+/**
+ * @author Carrie Hunner
+ * This class creates the pane displaying the default types
+ * provided
+ */
+public class DefaultTypesPane extends VBox{
     private EntityMenu myEntityMenu;
     private ResourceBundle myResources;
 
     private static final String RESOURCE = "default_entity_type";
-    private static final String TITLE_KEY = "Title";
+    private static final String TITLE_KEY = "DefaultTitle";
     private static final String TABS = "Tabs";
     private static final String TABS_REGEX = ",";
 
-    private static final int HEIGHT = 500;
-    private static final int WIDTH = 300;
-
-    public NewEntityTypePane()     {
+    /**
+     * Creates a new pane to display the default types
+     */
+    public DefaultTypesPane()     {
         myResources = ResourceBundle.getBundle(RESOURCE);
-        myEntityMenu = new EntityMenu(myResources.getString(TITLE_KEY), WIDTH, HEIGHT);
+        myEntityMenu = new EntityMenu(myResources.getString(TITLE_KEY));
         populateEntityMenu();
         this.getChildren().add(myEntityMenu);
     }
@@ -31,18 +34,17 @@ public class NewEntityTypePane extends VBox{
     private void populateEntityMenu(){
         String[] tabNames = myResources.getString(TABS).split(TABS_REGEX);
         for(String s1 : tabNames){
-            ArrayList<HBox> labelsList = new ArrayList<>();
+            ArrayList<Pane> labelsList = new ArrayList<>();
             String[] tabContentKeys = myResources.getString(s1).split(TABS_REGEX);
             for(String s2 : tabContentKeys){
                 System.out.println(s2);
                 String[] contentDefaultInfo = myResources.getString(s2).split(TABS_REGEX);
                 Label label = new Label(contentDefaultInfo[0]);
-                HBox hbox = new HBox(label);
-                labelsList.add(hbox);
+                Pane pane = new Pane(label);
+                labelsList.add(pane);
             }
-            VBox vBox = new VBox();
-            vBox.getChildren().addAll(labelsList);
-            myEntityMenu.addDropDown(s1, vBox);
+            myEntityMenu.addDropDown(s1);
+            myEntityMenu.addToDropDown(s1, labelsList);
         }
 
     }
