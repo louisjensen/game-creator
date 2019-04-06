f6df664681696aa07600630e45df1a085a9f0535
Lucas Liu <lucas.liu@duke.edu>
2019-04-06 10:56:20 -0400

    Skeleton set of Engine implementation, including a base set of components & systems. Also includes the introduction of Events

--- engine/src/engine/external/SoundComponent.java
+++
@@ -1,8 +0,0 @@
-package engine.external;
-
-public class SoundComponent<String> extends Component<String> {
-
-    public SoundComponent(String pathname){
-        super(pathname);
-    }
-}
---
+++ engine/src/engine/external/component/DirectionComponent.java
@@ -0,0 +1,8 @@
+package engine.external.component;
+
+public class DirectionComponent extends Component<Integer> {
+
+    public DirectionComponent (Integer value){
+        super(value);
+    }
+}
