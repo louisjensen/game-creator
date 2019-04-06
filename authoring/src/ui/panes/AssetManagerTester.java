7dc117987bc158bd44ca147d74be0dc1c4d08506
Carrie Hunner <clh87@duke.edu>
2019-04-06 11:06:21 -0400

    #52 updated AssetManagerTester to display some of what Ive been working on

--- authoring/src/ui/panes/AssetManagerTester.java
+++ authoring/src/ui/panes/AssetManagerTester.java
@@ -7,21 +7,27 @@ import ui.ErrorBox;
 import ui.TestEntity;
 import ui.UIException;
 import ui.manager.AssetManager;
+import ui.manager.LabelManager;
+import ui.manager.ObjectManager;
 
 public class AssetManagerTester extends Application {
 
     @Override
     public void start(Stage testStage) {
-        //AssetManager temp = new AssetManager(new TestEntity());
-        CreateNewObjectTypePane newObjectTypePane = new CreateNewObjectTypePane("Obstacles", "Platform");
-        NewEntityTypePane entityTypePane = new NewEntityTypePane();
+        //AssetManager temp = new AssetManager();
+        //temp.show();
+        CreateNewTypePane newObjectTypePane = new CreateNewTypePane("Obstacles", "Platform");
+        newObjectTypePane.show();
+        DefaultTypesPane entityTypePane = new DefaultTypesPane();
+        entityTypePane.setPrefSize(300, 600);
         Scene scene = new Scene(entityTypePane, 600, 600);
+        //scene.getStylesheets().add("default.css");
         testStage.setScene(scene);
         testStage.show();
-        Stage viewerStage = new Stage();
-        Scene viewerScene = new Scene(new Viewer(1000, 1000), 500, 500);
-        viewerStage.setScene(viewerScene);
-        viewerStage.show();
+        //Stage viewerStage = new Stage();
+        //Scene viewerScene = new Scene(new Viewer(1000, 1000), 500, 500);
+        //viewerStage.setScene(viewerScene);
+        //viewerStage.show();
     }
 
     public static void main(String[] args) {
