5e4c4a70db1ada8c12b0f382272b285d13d1e241
Megan Phibbons <mep76@duke.edu>
2019-04-07 17:20:40 -0400

    start on #79 by creating a GameCenterData object

---
+++ runner/src/runner/external/GameCenterData.java
@@ -0,0 +1,11 @@
+package runner.external;
+
+public class GameCenterData {
+    private String folderName;
+    private String gameTitle;
+    private String gameImageLocation;
+    private String gameDescription;
+
+    
+
+}
