8a3787252544cd8782706e9b01657fbcedc64bd6
Louis Jensen <lbj7@duke.edu>
2019-04-08 10:53:14 -0400

    writing game class #47

--- runner/src/runner/external/Game.java
+++ runner/src/runner/external/Game.java
@@ -2,6 +2,7 @@ package runner.external;
 
 import engine.external.Level;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Game {
@@ -9,5 +10,26 @@ public class Game {
     private String myIconName;
     private String myGameName;
     private String myGameDescription;
+    private int myWidth;
+    private int myHeight;
 
+    public Game(){
+        myLevels = new ArrayList<Level>();
+    }
+
+    public void addLevel(Level level){
+        myLevels.add(level);
+    }
+
+    public Collection<Level> getLevels(){
+        return myLevels;
+    }
+
+    public int getWidth(){
+        return myWidth;
+    }
+
+    public int getHeight(){
+        return myHeight;
+    }
 }
