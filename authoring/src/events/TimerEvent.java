67558c63c6d8e2461ff274770f13be6c1e1c5dd7
Lucas Liu <lucas.liu@duke.edu>
2019-04-09 21:20:03 -0400

    Merge branch 'TimerEvent' into 'engine'
    Added timer constructor that takes in a string for timerValue
    
    See merge request compsci308_2019spring/voogasalad_byteme!56

--- authoring/src/events/TimerEvent.java
+++ authoring/src/events/TimerEvent.java
@@ -5,10 +5,26 @@ import conditions.EqualToCondition;
 import engine.external.component.TimerComponent;
 
 public class TimerEvent extends Event {
+
+    /**
+     * Class constructor. Makes a new timer event given name of the entity and the Double timerValue
+     * @param name the String name of the entity that the timerEvent will affect
+     * @param timerValue Double representing the time at which actions associated with time will occur
+     */
     public TimerEvent(String name, Double timerValue) {
         super(name);
-
         makeTimerCondition(timerValue);
+    }
+
+    /**
+     * Class constructor. Makes a new timer event given name of the entity and the String timerValue
+     * @param name the String name of the entity that the timerEvent will affect
+     * @param timerValue String representing the time at which actions associated with time will occur
+     */
+    public TimerEvent(String name, String timerValue) {
+        super(name);
+        Double value = Double.parseDouble(timerValue);
+        makeTimerCondition(value);
     }
 
     private void makeTimerCondition(Double timerValue){
