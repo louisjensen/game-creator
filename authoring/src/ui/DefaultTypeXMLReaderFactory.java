c20b01b7df2ea7533b74244c4287f83f66924c67
Carrie Hunner <clh87@duke.edu>
2019-04-18 09:50:46 -0400

    #107 information retrieved from XMLs, next step is reflection and actually creating the components and entities

--- authoring/src/ui/DefaultTypeXMLReaderFactory.java
+++ authoring/src/ui/DefaultTypeXMLReaderFactory.java
@@ -1,27 +1,66 @@
 package ui;
 
+import engine.external.Entity;
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 public class DefaultTypeXMLReaderFactory {
-    private static final ResourceBundle RESOURCES = ResourceBundle.getBundle("authoring_general");
-    private static final String FOLDER_PATH = RESOURCES.getString("xml_folder_filepath");
+    private static final ResourceBundle PATH_RESOURCES = ResourceBundle.getBundle("authoring_general");
+    private static final ResourceBundle RESOURCES = ResourceBundle.getBundle("default_types_factory");
+    private static final String FOLDER_PATH = PATH_RESOURCES.getString("xml_folder_filepath");
     private static final String EXTENSIONS = ".xml";
-    private Map<String, Document> myNameToFileMap;
+    private Map<String, String> myNameToCategory;
+    private Map<String, Map<String, String>> myNameToComponents;    //Name of default -> (map from components names -> values)
+    private List<Node> myRootsList;
 
     public DefaultTypeXMLReaderFactory(){
-        initializeDocuments();
+        myNameToCategory = new HashMap<>();
+        myNameToComponents= new HashMap<>();
+        myRootsList = new ArrayList<>();
+        fillRootsList();
+        fillMaps();
+    }
+
+    /**
+     * @return List of possible default names
+     */
+    public List<String> getDefaultNames(){
+        List<String> defaultNames = new ArrayList<>(myNameToCategory.keySet());
+        return Collections.unmodifiableList(defaultNames);
+    }
+
+    /**
+     * @param name of the default type
+     * @return String of its category
+     */
+    public String getCategory(String name){
+        return myNameToCategory.get(name);
+    }
+
+    private void createEntity(String name){
+        Map<String, String> componentMap = myNameToComponents.get(name);
+
+    }
+    private void fillMaps() {
+        for(int k = 0; k < myRootsList.size(); k++){
+            Element currentElement = (Element) myRootsList.get(k);
+            Map<String, String> componentsMap = fillComponentsMap(currentElement);
+            if(hasRequiredInformation(componentsMap, currentElement)){
+                String name = componentsMap.get("Name");
+                myNameToComponents.put(name, componentsMap);
+                NodeList categoryList = currentElement.getElementsByTagName("Category");
+                String category = categoryList.item(categoryList.getLength()-1).getTextContent();
+                myNameToCategory.put(name, category);
+            }
+        }
     }
 
 
-    private void initializeDocuments(){
+    private void fillRootsList(){
         File assetFolder = new File(FOLDER_PATH);
         File[] files = assetFolder.listFiles((dir, name) -> name.toLowerCase().endsWith(EXTENSIONS));
         for(File temp : files) {
@@ -34,37 +73,46 @@ public class DefaultTypeXMLReaderFactory {
                 Document document = documentBuilder.parse(temp);
                 document.getDocumentElement().normalize();
                 Node root = document.getDocumentElement();
-                System.out.println("Root element :" + root.getNodeName());
-                NodeList nList = document.getElementsByTagName("Components");
-                Node nNode = nList.item(0);
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
-                NodeList components = ((Element) root).getElementsByTagName("Components");
-                for(int k = 0; k < components.getLength(); k++) {
-                    Node currentComponentList = components.item(k);
-                    NodeList subComponentsList = currentComponentList.getChildNodes();
-                    for (int i = 0; i < subComponentsList.getLength(); i++) {
-                        Node node = subComponentsList.item(i);
-                        if(node.getNodeType() == Node.ELEMENT_NODE){
-                            System.out.println(node.getNodeName() + "\t" + node.getTextContent());
-                        }
-                    }
-                }
-//
-
-
-            } catch (ParserConfigurationException e) {
-                e.printStackTrace();
-            } catch (SAXException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+                myRootsList.add(root);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
-
-
-
-
+        }
     }
+
+    private boolean hasRequiredInformation(Map<String, String> componentsMap, Element root) {
+        if(!componentsMap.containsKey("NameComponent")){
+            String[] info = RESOURCES.getString("NoName").split(",");
+            ErrorBox errorBox = new ErrorBox(info[0], info[1]);
+            errorBox.display();
+            return false;
+        }
+        else if(root.getElementsByTagName("Category").getLength() == 0){
+            String[] info = RESOURCES.getString("NoCategory").split(",");
+            ErrorBox errorBox = new ErrorBox(info[0], info[1]);
+            errorBox.display();
+            return false;
+        }
+        return true;
+    }
+
+    private Map<String, String> fillComponentsMap(Element root) {
+        NodeList components = root.getElementsByTagName("Components");
+        Map<String, String> componentsMap = new HashMap<>();
+        for(int k = 0; k < components.getLength(); k++) {
+            Node currentComponentList = components.item(k);
+            NodeList subComponentsList = currentComponentList.getChildNodes();
+            for (int i = 0; i < subComponentsList.getLength(); i++) {
+                Node node = subComponentsList.item(i);
+                if(node.getNodeType() == Node.ELEMENT_NODE){
+                    String componentName = node.getNodeName();
+                    String componentValue = node.getTextContent();
+                    componentsMap.put(componentName, componentValue);
+                }
             }
+        }
+        return componentsMap;
+    }
+
+
 }
