4d83bc91f127f99ddceabbc6f2608cef24f97dc0
Louis Jensen <lbj7@duke.edu>
2019-04-21 19:26:25 -0400

    Added MainEnityComponent #142

---
+++ engine/src/engine/external/component/MainEntityComponent.java
@@ -0,0 +1,7 @@
+package engine.external.component;
+
+public class MainEntityComponent extends Component<Boolean> {
+    public MainEntityComponent(Boolean progress){
+        super(progress);
+    }
+}
