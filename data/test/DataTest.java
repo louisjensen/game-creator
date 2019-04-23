2fd274bbb1151d0686fe0364dfeadec323689a77
Ryan Culhane <rpc21@duke.edu>
2019-04-23 02:21:35 -0400

    Added basic testing that demonstrates how to use the data module #150

--- data/test/DataTest.java
+++ data/test/DataTest.java
@@ -1,49 +1,89 @@
+import data.external.DataManager;
 import data.external.DatabaseEngine;
 import data.internal.UserQuerier;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DataTest {
 
-    private String userName1;
-    private String userName2;
-    private String correctPassword;
-    private String incorrectPassword;
-    private String fakeGameName1;
-    private String fakeGameName2;
-    private String fakeGameData1;
-    private String fakeGameData2;
-    private String fakeGameInfo1;
-    private String fakeGameInfo2;
+    private String myUserName1;
+    private String myUserName2;
+    private String myCorrectPassword;
+    private String myIncorrectPassword;
+    private String myFakeGameName1;
+    private String myFakeGameName2;
+    private String myFakeGameData1;
+    private DataManager myDataManager;
 
     @BeforeEach
     protected void setUp(){
+        DatabaseEngine.getInstance().open();
+        myDataManager = new DataManager();
         instantiateVariables();
         clearDatabase();
+    }
 
+    @AfterEach
+    protected void closeResources() {
+        clearDatabase();
+        DatabaseEngine.getInstance().close();
     }
 
     private void clearDatabase() {
-        DatabaseEngine.getInstance().open();
+        try {
+            myDataManager.removeGame(myFakeGameName1, myUserName1);
+            myDataManager.removeGame(myFakeGameName1, myUserName2);
+            myDataManager.removeGame(myFakeGameName2, myUserName1);
+            myDataManager.removeGame(myFakeGameName2, myUserName2);
+            myDataManager.removeUser(myUserName1);
+            myDataManager.removeUser(myUserName2);
+        } catch (SQLException exception) {
+            // just debugging the test cases, does not get included
+            exception.printStackTrace();
+        }
 
     }
 
     private void instantiateVariables() {
-        userName1 = "userName1";
-        userName2 = "userName2";
-        correctPassword = "correctPassword";
-        incorrectPassword = "incorrectPassword";
-        fakeGameName1 = "fakeGameName1";
-        fakeGameName2 = "fakeGameName2";
-        fakeGameData1 = "fakeGameData1";
-        fakeGameData2 = "fakeGameData2";
-        fakeGameInfo1 = "fakeGameInfo1";
-        fakeGameInfo2 = "fakeGameInfo2";
+        myUserName1 = "userName1";
+        myUserName2 = "userName2";
+        myCorrectPassword = "correctPassword";
+        myIncorrectPassword = "incorrectPassword";
+        myFakeGameName1 = "fakeGameName1";
+        myFakeGameName2 = "fakeGameName2";
+        myFakeGameData1 = "fakeGameData1";
     }
 
     @Test
     public void testCreateUser(){
+        assertTrue(myDataManager.createUser(myUserName1, myCorrectPassword));
+        assertFalse(myDataManager.createUser(myUserName1, myIncorrectPassword));
+    }
 
+    @Test
+    public void testValidateUser() {
+        assertTrue(myDataManager.createUser(myUserName1, myCorrectPassword));
+        assertTrue(myDataManager.validateUser(myUserName1, myCorrectPassword));
+        assertFalse(myDataManager.validateUser(myUserName1, myIncorrectPassword));
+        assertFalse(myDataManager.validateUser(myUserName2, myCorrectPassword));
+    }
+
+    @Test
+    public void testSaveAndLoadGameData() {
+        myDataManager.saveGameData(myFakeGameName1, myUserName1, myFakeGameData1);
+        String loadedData = (String) myDataManager.loadGameData(myFakeGameName1);
+        assertEquals(loadedData, myFakeGameData1);
+    }
+
+    @Test
+    public void testInvalidConnection() {
+        DatabaseEngine.getInstance().close();
+        assertThrows(SQLException.class, () -> myDataManager.removeUser(myUserName1));
+        DatabaseEngine.getInstance().open();
     }
 }
