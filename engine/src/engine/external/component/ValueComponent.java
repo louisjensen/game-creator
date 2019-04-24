877d15b5d601fe459d53de5d412750ece70a5f98
Lucas Liu <ltl9@duke.edu>
2019-04-23 20:47:02 -0400

    valueComponent implementation

--- engine/src/engine/external/component/ValueComponent.java
+++ engine/src/engine/external/component/ValueComponent.java
@@ -1,6 +1,10 @@
 package engine.external.component;
 
-@Deprecated
+/**
+ * @author Lucas Liu
+ * @param <T>
+ *     Useful component for storing a value which is custom to a particular game. One could store a double, String, or even another Entity
+ */
 public class ValueComponent<T> extends Component<T> {
 
     public ValueComponent(T t) {
