7a36fd51e584f57ac7c457a414b91cd6b446f15d
Lucas Liu <ltl9@duke.edu>
2019-04-24 16:37:06 -0400

    synchronized updates per frame

--- engine/src/engine/external/Engine.java
+++ engine/src/engine/external/Engine.java
@@ -49,7 +49,7 @@ public class Engine {
      * @param inputs collection of user Keycode inputs received on this game loop
      * @return all game Entities after being updated by Systems in current game loop
      */
-    public Collection<Entity> updateState(Collection<KeyCode> inputs){
+    public synchronized Collection<Entity> updateState(Collection<KeyCode> inputs){
         for(int i = 0; i<SYSTEM_ORDER_RESOURCES.keySet().size(); i++){
             mySystems.get(i).update(myEntities,inputs);
         }
