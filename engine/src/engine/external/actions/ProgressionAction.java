c5e7d5217eb9e81525fb74405b706623062188d9
Louis Jensen <lbj7@duke.edu>
2019-04-25 21:33:50 -0400

    changing name of variables to match class name

--- engine/src/engine/external/actions/ProgressionAction.java
+++ engine/src/engine/external/actions/ProgressionAction.java
@@ -1,10 +1,9 @@
 package engine.external.actions;
 
-import engine.external.component.DestroyComponent;
 import engine.external.component.ProgressionComponent;
 
 public class ProgressionAction extends BooleanAction {
-    public ProgressionAction(Boolean destroy){
-        setAction(destroy, ProgressionComponent.class);
+    public ProgressionAction(Boolean progress){
+        setAction(progress, ProgressionComponent.class);
     }
 }
