f32790b4c6b014da84e9dea6679c76ef795eea9b
Carrie Hunner <clh87@duke.edu>
2019-04-26 19:59:15 -0400

    #163 commented TreeNode class

--- authoring/src/ui/TreeNode.java
+++ authoring/src/ui/TreeNode.java
@@ -7,21 +7,39 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * @author Carrie Hunner
+ * This class was created for the purpose of arranging asset files based on name, as the database has one table.
+ * Each node is capable of storing a name as well as list of its children that are files (these are leaf nodes)
+ * and its children that are TreeNodes.
+ * The String name associated with the Node is used for different display purposes.
+ */
 public class TreeNode {
     private String myName;
     private Set<File> myFileChildren;
     private Set<TreeNode> myTreeNodeChildren;
 
+    /**
+     * @param name String associated with the TreeNode
+     */
     public TreeNode(String name){
         myName = name;
         myFileChildren = new HashSet<>();
         myTreeNodeChildren = new HashSet<>();
     }
 
+    /**
+     * Adds a TreeNode child
+     * @param child TreeNode to be a child of this node
+     */
     public void addChild(TreeNode child){
         myTreeNodeChildren.add(child);
     }
 
+    /**
+     * Adds a File to be a leaf node child of this node
+     * @param child
+     */
     public void addChild(File child){
         myFileChildren.add(child);
     }
@@ -43,16 +61,26 @@ public class TreeNode {
         return newTreeNode;
     }
 
-
+    /**
+     * @return String name associated with this node
+     */
     public String getName(){
         return  myName;
     }
 
+    /**
+     * Gets the File leaf node children associated with this node
+     * @return List<File>
+     */
     public List<File> getFileChildren(){
         List<File> fileList = new ArrayList<>(myFileChildren);
         return Collections.unmodifiableList(fileList);
     }
 
+    /**
+     * Gets the TreeNode children associated with this node
+     * @return LIst<TreeNode>
+     */
     public List<TreeNode> getNodeChildren(){
         List<TreeNode> nodeList = new ArrayList<>(myTreeNodeChildren);
         return Collections.unmodifiableList(nodeList);
