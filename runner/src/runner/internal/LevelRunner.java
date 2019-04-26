5e680326c271a7e57d6f023813ea7f376c9247f7
Louis Jensen <lbj7@duke.edu>
2019-04-26 16:24:17 -0400

    Refactoring systems

--- runner/src/runner/internal/LevelRunner.java
+++ runner/src/runner/internal/LevelRunner.java
@@ -3,7 +3,6 @@ package runner.internal;
 import engine.external.Engine;
 import engine.external.Entity;
 import engine.external.Level;
-import engine.external.component.*;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Group;
@@ -14,7 +13,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import runner.internal.runnerSystems.*;
-
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -22,26 +20,21 @@ public class LevelRunner {
     private PauseButton myPauseButton;
     private Node myPause;
     private Collection<Entity> myEntities;
-    private int mySceneWidth;
-    private int mySceneHeight;
-    private Stage myStage;
-    private Group myGroup;
-    private Scene myScene;
+    protected int mySceneWidth;
+    protected int mySceneHeight;
+    protected Stage myStage;
+    protected Group myGroup;
+    protected Scene myScene;
     private Engine myEngine;
-    private Timeline myAnimation;
+    protected Timeline myAnimation;
     private static final int FRAMES_PER_SECOND = 60;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     private Level myLevel;
     private Set<KeyCode> myCurrentKeys;
     private boolean canPause = false;
-    private Consumer<Double> myLevelChanger;
-    private ProgressionSystem myProgressionSystem;
-    private ScrollingSystem myScrollingSystem;
-    private ImageDisplaySystem myImageDisplaySystem;
-    private ScoringSystem myScoringSystem;
-    private LivesSystem myLivesSystem;
-    private LevelSystem myLevelSystem;
+    protected Consumer<Double> myLevelChanger;
+    private List<RunnerSystem> mySystems;
 
     public LevelRunner(Level level, int width, int height, Stage stage, Consumer playNext){
         myLevel = level;
@@ -59,30 +52,8 @@ public class LevelRunner {
     }
 
     private void initializeSystems() {
-        Collection<Class<? extends Component>> systemComponents = new ArrayList<>();
-        systemComponents.add(ProgressionComponent.class);
-        systemComponents.add(NextLevelComponent.class);
-        myProgressionSystem = new ProgressionSystem(systemComponents, this,
-                myGroup, myStage, myAnimation, mySceneWidth, mySceneHeight, myLevelChanger);
-        systemComponents.clear();
-        systemComponents.add(CameraComponent.class);
-        myScrollingSystem = new ScrollingSystem(systemComponents, this, myGroup, myScene);
-
-        systemComponents.clear();
-        systemComponents.add(ImageViewComponent.class);
-        myImageDisplaySystem = new ImageDisplaySystem(systemComponents, this, myGroup);
-
-        Collection<Class<? extends Component>> comps = new ArrayList<>();
-        comps.add(ScoreComponent.class);
-        myScoringSystem = new ScoringSystem(comps, this);
-
-        Collection<Class<? extends Component>> comp1 = new ArrayList<>();
-        comp1.add(ScoreComponent.class);
-        myLivesSystem = new LivesSystem(comp1, this);
-
-        Collection<Class<? extends Component>> comp2 = new ArrayList<>();
-        comp2.add(NextLevelComponent.class);
-        myLevelSystem = new LevelSystem(comp2, this);
+        SystemManager systems = new SystemManager(this);
+        mySystems = systems.getSystems();
     }
 
     private void addPauseButton() {
@@ -126,12 +97,9 @@ public class LevelRunner {
 
     private void showEntities(){
         myGroup.getChildren().retainAll(myPause);
-        myProgressionSystem.update(myEntities);
-        myScrollingSystem.update(myEntities);
-        myImageDisplaySystem.update(myEntities);
-        myScoringSystem.update(myEntities);
-        myLivesSystem.update(myEntities);
-        myLevelSystem.update(myEntities);
+        for(RunnerSystem system : mySystems){
+            system.update(myEntities);
+        }
         if (canPause) movePauseButton();
     }
 
@@ -143,4 +111,3 @@ public class LevelRunner {
         return myEntities;
     }
 }
-
---
+++ runner/src/runner/internal/SystemManager.java
@@ -0,0 +1,49 @@
+package runner.internal;
+
+import engine.external.component.*;
+import runner.internal.runnerSystems.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class SystemManager {
+    private List<RunnerSystem> mySystems;
+    private LevelRunner myLevelRunner;
+
+    public SystemManager(LevelRunner levelRunner){
+        myLevelRunner = levelRunner;
+        mySystems = new ArrayList<>();
+        Collection<Class<? extends Component>> components = new ArrayList<>();
+        components.add(ProgressionComponent.class);
+        components.add(NextLevelComponent.class);
+        mySystems.add(new ProgressionSystem(components, myLevelRunner,
+                myLevelRunner.myGroup, myLevelRunner.myStage, myLevelRunner.myAnimation,
+                myLevelRunner.mySceneWidth, myLevelRunner.mySceneHeight, myLevelRunner.myLevelChanger));
+
+        Collection<Class<? extends Component>> components2 = new ArrayList<>();
+        components2.add(CameraComponent.class);
+        mySystems.add(new ScrollingSystem(components2, myLevelRunner, myLevelRunner.myGroup, myLevelRunner.myScene));
+
+        Collection<Class<? extends Component>> components3 = new ArrayList<>();
+        components3.add(ImageViewComponent.class);
+        mySystems.add(new ImageDisplaySystem(components3, myLevelRunner, myLevelRunner.myGroup));
+
+        Collection<Class<? extends Component>> components4 = new ArrayList<>();
+        components4.add(ScoreComponent.class);
+        mySystems.add(new ScoringSystem(components4, myLevelRunner));
+
+        Collection<Class<? extends Component>> components5 = new ArrayList<>();
+        components5.add(ScoreComponent.class);
+        mySystems.add(new LivesSystem(components5, myLevelRunner));
+
+        Collection<Class<? extends Component>> components6 = new ArrayList<>();
+        components6.add(NextLevelComponent.class);
+        mySystems.add(new LevelSystem(components6, myLevelRunner));
+    }
+
+    public List<RunnerSystem> getSystems(){
+        return mySystems;
+    }
+}
