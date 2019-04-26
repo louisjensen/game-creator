9a981daa941cf68240b462297d3424dd981da3e2
Harry Ross <hgr8@duke.edu>
2019-04-26 19:48:54 -0400

    Made progress on saving, adding components based on events #35

--- authoring/src/factory/GameTranslator.java
+++ authoring/src/factory/GameTranslator.java
@@ -3,19 +3,21 @@ package factory;
 import engine.external.Entity;
 import engine.external.Level;
 import engine.external.actions.Action;
+import engine.external.actions.SoundAction;
+import engine.external.actions.TimerAction;
+import engine.external.actions.ValueAction;
+import engine.external.component.CollisionComponent;
 import engine.external.component.Component;
-import engine.external.component.GroupComponent;
-import engine.external.component.HeightComponent;
-import engine.external.component.NameComponent;
-import engine.external.component.SpriteComponent;
-import engine.external.component.WidthComponent;
-import engine.external.component.XPositionComponent;
-import engine.external.component.YPositionComponent;
-import engine.external.component.ZPositionComponent;
+import engine.external.component.SoundComponent;
+import engine.external.component.TimerComponent;
+import engine.external.component.ValueComponent;
 import engine.external.conditions.Condition;
+import engine.external.events.BottomCollisionEvent;
 import engine.external.events.Event;
+import engine.external.events.LeftCollisionEvent;
+import engine.external.events.RightCollisionEvent;
+import engine.external.events.TopCollisionEvent;
 import runner.external.Game;
-import runner.external.GameCenterData;
 import ui.AuthoringEntity;
 import ui.AuthoringLevel;
 import ui.EntityField;
@@ -23,6 +25,7 @@ import ui.LevelField;
 import ui.manager.ObjectManager;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -72,26 +75,38 @@ public class GameTranslator {
         Entity basisEntity = new Entity();
 
         for (EntityField field : EntityField.values()) {
-            if (authEntity.getPropertyMap().containsKey(field) && !field.equals(EntityField.EVENTS) && !field.equals(EntityField.IMAGE)) {
+            if (authEntity.getPropertyMap().containsKey(field) && !field.equals(EntityField.CAMERA) && !field.equals(EntityField.EVENTS) && !field.equals(EntityField.IMAGE)) {
                     addComponent(field, basisEntity, authEntity);
                 }
+            else if (field.equals(EntityField.CAMERA) && Boolean.parseBoolean(authEntity.getPropertyMap().get(EntityField.CAMERA))) { // main character found
+                //basisEntity.addComponent(new LivesComponent());
+                //basisEntity.addComponent(new ScoreComponent(0.0));
+            }
         }
         // TODO do special image/sound thing
-        // TODO add components from Events? (Collision, ValueComponent)
 
+        //TODO group events?
         for (AuthoringLevel authLevel : myObjectManager.getLevels()) {
             for (AuthoringEntity entity : authLevel.getEntities()) {
                 for (Event event : myObjectManager.getEvents(entity.getPropertyMap().get(EntityField.LABEL))) {
-                    /*for (Action action : event.getActions()) {
-
+                    if (event.getClass().equals(BottomCollisionEvent.class) || event.getClass().equals(LeftCollisionEvent.class) ||
+                    event.getClass().equals(RightCollisionEvent.class) || event.getClass().equals(TopCollisionEvent.class)) {
+                    basisEntity.addComponent(new CollisionComponent(true));
+                    }        // TODO add components from Events? (Collision, ValueComponent)
+                    for (Action action : (List<Action>) event.getEventInformation().get(Action.class)) {
+                        if (action.getClass().equals(SoundAction.class) && !basisEntity.hasComponents(SoundComponent.class))
+                            basisEntity.addComponent(new SoundComponent("")); //TODO
+                        if (action.getClass().equals(TimerAction.class) && !basisEntity.hasComponents(TimerComponent.class))
+                            basisEntity.addComponent(new TimerComponent(0));
+                        if (action.getClass().equals(ValueAction.class) && !basisEntity.hasComponents(ValueComponent.class))
+                            basisEntity.addComponent(new ValueComponent<>(0));
                     }
-                    for (Condition condition : event.getConditions()) {
-
-                    }*/
+                    for (Condition condition : (List<Condition>) event.getEventInformation().get(Condition.class)) {
+                        //TODO what even
+                    }
                 }
             }
         }
-
         return basisEntity;
     }
 
