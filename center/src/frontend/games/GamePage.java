fd099b6a1059ab30a53b8d660ddec01cb70ac1da
Megan Phibbons <mep76@duke.edu>
2019-04-26 23:22:02 -0400

    work on #123 by implementing some of the GamePage display

--- center/src/frontend/games/GamePage.java
+++ center/src/frontend/games/GamePage.java
@@ -1,17 +1,18 @@
 package frontend.games;
 
 import data.external.GameCenterData;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class GamePage {
     private GameCenterData myData;
-    private Pane myDisplay;
-    private static final String TEXT_SELECTOR = "cardtext";
-    private static final String TITLE_SELECTOR = "cardtitle";
+    private BorderPane myDisplay;
+    private static final String TITLE_SELECTOR = "titlefont";
+    private static final String TEXT_SELECTOR = "subtitlefont";
     private static final double STAGE_WIDTH = 1250;
     private static final double STAGE_HEIGHT = 750;
     private static final Color BACKGROUND_COLOR = Color.rgb(46, 43, 51);
@@ -27,19 +28,24 @@ public class GamePage {
 
     private void initializeDisplay() {
         myDisplay = new BorderPane();
-        addHeader(myDisplay);
-        addBody(myDisplay);
+        myDisplay.getStylesheets().add("center.css");
+        addHeader();
+        addBody();
     }
 
-    private void addBody(Pane myDisplay) {
+    private void addBody() {
 
     }
 
-    private void addHeader(Pane myDisplay) {
+    private void addHeader() {
         Text title = new Text(myData.getTitle());
         title.getStyleClass().add(TITLE_SELECTOR);
         Text author = new Text(myData.getAuthorName());
         author.getStyleClass().add(TEXT_SELECTOR);
+        VBox text = new VBox(title, author);
+        text.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(text, Pos.CENTER);
+        myDisplay.setTop(text);
     }
 
 }
