94f845cc50e12182d121f7aa91cee36ffed3051c
Megan Phibbons <mep76@duke.edu>
2019-04-26 20:53:31 -0400

    work on #123 by creating a GamePage object

---
+++ center/src/frontend/games/GamePage.java
@@ -0,0 +1,32 @@
+package frontend.games;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import runner.external.GameCenterData;
+
+public class GamePage {
+    private GameCenterData myData;
+    private Pane myDisplay;
+
+    public GamePage(GameCenterData data) {
+        myData = data;
+        initializeDisplay();
+    }
+
+    private void initializeDisplay() {
+        myDisplay = new BorderPane();
+        addHeader(myDisplay);
+        addBody(myDisplay);
+    }
+
+    private void addBody(Pane myDisplay) {
+
+    }
+
+    private void addHeader(Pane myDisplay) {
+        Text title = new Text(myData.getTitle());
+
+    }
+
+}
