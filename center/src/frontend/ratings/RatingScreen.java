6d4b70d87082ca45ffc91d3ad27be91f1d913089
Megan Phibbons <mep76@duke.edu>
2019-04-27 18:11:41 -0400

    finish up front-end for #125 and moving into work on the backend and button implementation

--- center/src/frontend/ratings/RatingScreen.java
+++ center/src/frontend/ratings/RatingScreen.java
@@ -3,6 +3,7 @@ package frontend.ratings;
 import data.external.GameCenterData;
 import frontend.Utilities;
 import javafx.geometry.Pos;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
@@ -10,9 +11,11 @@ import java.util.ResourceBundle;
 
 public class RatingScreen {
     private static final double RATING_WIDTH = 350;
-    private static final double RATING_HEIGHT = 400;
+    private static final double RATING_HEIGHT = 450;
     private static final String DEFAULT_LANGUAGE = "languages/English";
     private static final String BODY_SELECTOR = "bodyfont";
+    private static final String TEXT_FIELD_SELECTOR = "commentbox";
+    private static final String PADDING_SELECTOR = "ratingpadding";
 
     private BorderPane myDisplay;
     private ResourceBundle myLanguageBundle;
@@ -44,7 +47,9 @@ public class RatingScreen {
         BorderPane ratingOptions = new BorderPane();
         addStars(ratingOptions);
         addCommentBox(ratingOptions);
+        ratingOptions.getStyleClass().add(PADDING_SELECTOR);
         myDisplay.setCenter(ratingOptions);
+        myDisplay.getStyleClass().add(PADDING_SELECTOR);
     }
 
     private void addButtons() {
@@ -60,11 +65,20 @@ public class RatingScreen {
         StarBox visualStars = new StarBox();
         BorderPane.setAlignment(visualStars.getDisplay(), Pos.CENTER);
         stars.setCenter(visualStars.getDisplay());
+        stars.getStyleClass().add(PADDING_SELECTOR);
         pane.setTop(stars);
     }
 
-    private void addCommentBox(Pane pane) {
-//
+    private void addCommentBox(BorderPane pane) {
+        BorderPane comment = new BorderPane();
+        Text commentTitle = new Text(Utilities.getValue(myLanguageBundle, "commentTitle"));
+        commentTitle.getStyleClass().add(BODY_SELECTOR);
+        BorderPane.setAlignment(commentTitle, Pos.CENTER);
+        comment.setTop(commentTitle);
+        TextArea commentBox = new TextArea();
+        commentBox.getStyleClass().add(TEXT_FIELD_SELECTOR);
+        comment.setCenter(commentBox);
+        pane.setCenter(comment);
     }
 
     private void addTitle() {
