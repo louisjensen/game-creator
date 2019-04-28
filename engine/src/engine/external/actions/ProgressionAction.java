2ef22d9a3d8142a5ea5094276524ccbbf772c996
Feroze Mohideen <feroze.mohideen@duke.edu>
2019-04-28 17:47:24 -0400

    modified next level and progression actions so they add the components

--- engine/src/engine/external/actions/ProgressionAction.java
+++ engine/src/engine/external/actions/ProgressionAction.java
@@ -2,8 +2,8 @@ package engine.external.actions;
 
 import engine.external.component.ProgressionComponent;
 
-public class ProgressionAction extends BooleanAction {
+public class ProgressionAction extends AddComponentAction {
     public ProgressionAction(Boolean progress){
-        setAction(progress, ProgressionComponent.class);
+        setAbsoluteAction(new ProgressionComponent(progress));
     }
 }
