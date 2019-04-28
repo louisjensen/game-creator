5a3abd9c54fead69646544f98e230a8fdd77d831
Dima Fayyad <dima.fayyad@duke.edu>
2019-04-28 17:56:09 -0400

    Opacity default

--- engine/src/engine/external/component/OpacityComponent.java
+++ engine/src/engine/external/component/OpacityComponent.java
@@ -2,6 +2,12 @@ package engine.external.component;
 
 public class OpacityComponent extends Component<Double> {
 
+    private final static double DEFAULT = 1.0;
+
+    public OpacityComponent() {
+        super(DEFAULT);
+    }
+
     public OpacityComponent(Double value){
         super(value);
     }
