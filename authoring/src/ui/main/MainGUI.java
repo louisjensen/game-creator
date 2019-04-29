0de9b30f1bb63687c847346921a45cf51ec82f6e
Harry Ross <hgr8@duke.edu>
2019-04-28 21:00:49 -0400

    Fixed import issue #31

--- authoring/src/ui/main/MainGUI.java
+++ authoring/src/ui/main/MainGUI.java
@@ -43,6 +43,7 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
